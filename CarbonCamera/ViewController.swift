f5e8d1acaff051ea804af6098b918cb58a18b437
James Bungay <jtb.github@gmail.com>
2020-07-22 19:31:13 +0100

    Create iOS app Xcode project

---
+++ CarbonCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarbonCamera
+//
+//  Created by James Bungay on 22/07/2020.
+//  Copyright © 2020 James Bungay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
