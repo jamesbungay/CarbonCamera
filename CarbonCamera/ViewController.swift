12c1c89b6720ee888572aaa6497295dfac986327
James Bungay <jtb.github@gmail.com>
2020-07-24 23:31:07 +0100

    Add torch button to UI and set up basic CoreML model

--- CarbonCamera/ViewController.swift
+++ CarbonCamera/ViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 import AVFoundation
+import Vision  // Vision module of CoreML
+
+
+// TODO: Reset torch button image when app comes back into view after being suspended
+
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     @IBOutlet weak var cameraPreviewView: CameraPreviewView!
+    @IBOutlet weak var torchButton: UIButton!
+    
+    var deviceHasTorch: Bool = false
     
     let captureSession = AVCaptureSession()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +32,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         if AVCaptureDevice.authorizationStatus(for: .video) == .authorized || AVCaptureDevice.authorizationStatus(for: .video) == .notDetermined {
             self.setUpCaptureSession()
         }
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -52,6 +62,8 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
             else { return }  // Configuration failed, no back camera.
+        
+        self.deviceHasTorch = videoDevice.hasTorch
         
         guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice)
             else { return }  // Configuration failed, cannot use back camera as capture input device.
@@ -87,12 +99,63 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     
+    // MARK: Camera control button handler methods
+    
+    @IBAction func torchButtonTapped(_ sender: Any) {
+        
+        guard let videoDevice = AVCaptureDevice.default(for: .video)
+            else { return }
+        
+        guard videoDevice.hasTorch, videoDevice.isTorchAvailable
+            else { return }
+        
+        if videoDevice.torchMode != .on {  // Turn on torch and configure button to show this:
+            do {
+                try videoDevice.lockForConfiguration()
+                videoDevice.torchMode = .on
+                torchButton.setBackgroundImage(UIImage(systemName: "lightbulb"), for: UIControl.State.normal)
+            } catch { return }  // Video device could not be reconfigured to turn on torch.
+            
+        } else {  // Turn off torch and configure button to show this:
+            do {
+                try videoDevice.lockForConfiguration()
+                videoDevice.torchMode = .off
+                torchButton.setBackgroundImage(UIImage(systemName: "lightbulb.slash"), for: UIControl.State.normal)
+            } catch { return }  // Video device could not be reconfigured to turn off torch.
+        }
+        
+        videoDevice.unlockForConfiguration()  // Allow other apps to reconfigure video device.
+    }
+    
+    
     // MARK: CaptureSession Output Delegate Methods
     
     // Function called every time the camera captures a frame:
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        print("Camera captured frame on ", Date())
+        
+        // Set up CoreML vision model, request and handler method:
+        
+        guard let visionModel = try? VNCoreMLModel(for: Resnet50().model)
+            else { return }
+        
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: {
+            completedClassificationRequest, err in
+
+            guard let firstObservation = completedClassificationRequest.results?.first as? VNClassificationObservation
+                else { return }  // No results of classification
+            
+            print(firstObservation.identifier, " -- ", firstObservation.confidence)
+        })
+        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+            else { return }
+        
+        let bgQueue = DispatchQueue(label: "queue.serial.classificationRequestHandler")
+        bgQueue.async {
+            let requestHandler = VNImageRequestHandler(cvPixelBuffer: imageBuffer, options: [:])
+            try? requestHandler.perform([classificationRequest])
+        }
     }
     
     
