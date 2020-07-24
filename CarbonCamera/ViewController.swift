1ac55ab13d95b8e49eedc9145453687e317cdf9c
James Bungay <jtb.github@gmail.com>
2020-07-24 15:02:45 +0100

    Set up video captureSession and display preview on screen

--- CarbonCamera/ViewController.swift
+++ CarbonCamera/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraPreviewView: CameraPreviewView!
+    
+    let captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // MARK: Setup video captureSession
+        
+        // Setup video input to captureSession:
+        
+        captureSession.beginConfiguration()
+        
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            else { return }  // Configuration failed, no back camera.
+        
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice)
+            else { return }  // Configuration failed, cannot use back camera as capture input device.
+        
+        if captureSession.canAddInput(videoDeviceInput) {
+            captureSession.addInput(videoDeviceInput)
+        } else { return }  // Configuration failed, cannot add input to captureSession.
+        
+        // Setup video output from captureSession:
+        
+        let videoDataOutput = AVCaptureVideoDataOutput()  // Continuous video data output
+        
+        if captureSession.canSetSessionPreset(.high) {
+            captureSession.sessionPreset = .high
+        } else { return }
+        
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+        } else { return }
+        
+        // Setup preview for captureSession:
+        cameraPreviewView.videoPreviewLayer.session = captureSession
+        cameraPreviewView.videoPreviewLayer.videoGravity = .resizeAspectFill  // Set video preview to fill the view
+        
+        captureSession.commitConfiguration()
+        let serialQueue = DispatchQueue(label: "com.queue.serial")
+        serialQueue.async {
+            self.captureSession.startRunning()
+        }
     }
 
 
